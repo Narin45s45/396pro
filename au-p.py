facb7c5c888c1456778ef58fd86e5523d0b42364
Narin45s45 <155718259+Narin45s45@users.noreply.github.com>
2025-04-28 00:30:59 +0330

    Update au-p.py

--- au-p.py
+++ au-p.py
@@ -18,7 +18,7 @@ RSS_FEED_URL = "https://www.newsbtc.com/feed/"
 GEMINI_API_KEY = os.environ.get("GEMAPI")
 if not GEMINI_API_KEY:
     raise ValueError("!متغیر محیطی GEMAPI پیدا نشد")
-GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent"
+GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/Gemini-2.5-Flash-Preview-04-17:generateContent"
 
 creds_json = os.environ.get("CREDENTIALS")
 if not creds_json:
