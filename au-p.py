424f7f732a94ced5dd514b44a705bc7e68e02747
Narin45s45 <155718259+Narin45s45@users.noreply.github.com>
2025-07-09 23:04:25 +0330

    Update au-p.py

--- au-p.py
+++ au-p.py
@@ -56,15 +56,15 @@ WORDPRESS_PASS = os.environ.get("WORDPRESS_PASS")
 IMAGE_PROXY_URL = os.environ.get("IMAGE_PROXY_URL") # <-- این خط را اینجا اضافه کنید
 
 # URLهای Endpointهای API شما
-WORDPRESS_CUSTOM_POST_API_ENDPOINT = f"{WORDPRESS_URL}/wp-json/my-poster/v1/create"
-WORDPRESS_PROCESSED_LINKS_GET_API_ENDPOINT = f"{WORDPRESS_URL}/wp-json/my-poster/v1/processed-links"
-WORDPRESS_PROCESSED_LINKS_ADD_API_ENDPOINT = f"{WORDPRESS_URL}/wp-json/my-poster/v1/processed-links"
+WORDPRESS_CUSTOM_POST_API_ENDPOINT = f"{WORDPRESS_MAIN_URL}/wp-json/my-poster/v1/create"
+WORDPRESS_PROCESSED_LINKS_GET_API_ENDPOINT = f"{WORDPRESS_MAIN_URL}/wp-json/my-poster/v1/processed-links"
+WORDPRESS_PROCESSED_LINKS_ADD_API_ENDPOINT = f"{WORDPRESS_MAIN_URL}/wp-json/my-poster/v1/processed-links"
 
 REQUEST_TIMEOUT = 60
 GEMINI_TIMEOUT = 150
 MASTER_LOG_FILE = "master_log.txt"
 
-if not all([GEMAPI, WORDPRESS_URL, WORDPRESS_USER, WORDPRESS_PASS, IMAGE_PROXY_URL]):
+if not all([GEMINI_API_KEY, WORDPRESS_MAIN_URL, WORDPRESS_USER, WORDPRESS_PASS, IMAGE_PROXY_URL]):
     raise ValueError("یکی از متغیرهای محیطی ضروری (GEMAPI, WORDPRESS_URL, WORDPRESS_USER, WORDPRESS_PASS, IMAGE_PROXY_URL) تنظیم نشده است.")
 
 # --- توابع کمکی ---
